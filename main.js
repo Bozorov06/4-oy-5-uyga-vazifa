c4171cddc1fe90ec9414ee91f86ddb4b6cd7fe3c
Bozorov06 <qoyilbekbozorov160@gmail.com>
2024-11-29 17:21:20 +0500

    ulandi

---
+++ main.js
@@ -0,0 +1,377 @@
+// 1-misol
+// for( let i = 1; i<= 100; i++){
+//     console.log (i)
+// }
+
+// 2-misol
+// for( let i = 1; i<=50; i++){
+//     if (i%2==0){
+//         console.log(i)
+//     }
+// }
+
+// 3-misol
+// for (let i =1 ; i<=30; i++){
+//     if (i%2==1){
+//         console.log(i);
+//     }
+// }
+
+// 4-misol
+// for (let i = 1; i<=20; i++){
+//     console.log(i**2);
+// }
+
+// 5-misol
+// for (let i = 10; i>=1; i--){
+//     console.log(i);   
+// }
+
+// 6-misol
+// let sum =0;
+// for (let i=1; i<=100; i++){
+//     sum= sum+i
+// }
+//  console.log(sum)
+
+// 7-misol
+// let sum = 0;
+// for (let i = 1; i<=20; i++){
+//     if (i%2==0)
+//         sum= sum+i
+// }
+
+// console.log(sum);
+
+// 8-misol
+// let k = 1;
+// for(let i = 1; i<=15; i++){
+//     if (i%2==1)
+//         k= k*i
+// }
+
+// console.log(k)
+
+// 9-misol
+// for (let i = 5; i<=25;   i++) {
+//    console.log(i**3)
+// }
+
+// 10-misol
+// for (let i = 1; i<=10; i++){
+//     console.log(Math.pow(i, 1/2));
+// }
+
+// 11-misol
+// for(let i = 1; i<=50; i++){
+//     if (i%3==0){
+//         console.log(i);
+//     }
+// }
+
+// 12-misol
+// let sum = 0;
+// for (let i = 1; i<=100;  i++){
+//     sum=sum+i
+// }
+// console.log(sum);
+
+// 13-misol
+// for (let i = 1; i<=100;  i++){
+//     if (i%7==0){
+//         console.log(i);
+//     }
+// }
+
+// 14-misol
+// let sum = 0;
+// for (let i = 1; i <= 100; i++) {
+//   sum = sum + i;
+// }
+// console.log(sum%100);
+
+// 15-misol
+// let sum = 0;
+// for (let i = 1; i<=20; i++){
+//     if (i%2==1)
+//         sum= sum+i
+// }
+
+// console.log(sum);
+
+// 16-misol
+// let son = +prompt("sonni kirting")
+// for (let i = 2; i<=son; i+=2){
+//     console.log(i);
+// }
+
+// 17-misol
+// for (let i = 1; i<=50; i++){
+//     if (i%5==0 && i%3==0){
+//         console.log(i);
+//     }
+// }
+
+// 18-misol
+// let k = 1;
+// let sum = 0;
+// for (let i = 1; i<=30; i++){
+//     k = i ** 2;
+//     sum=sum+k
+// }
+// console.log(sum);
+
+// 19-misol
+// let counter= 0;
+// for (let i = 1; i<=100; i++){
+//     if (i%2==1)
+//         counter= counter+1
+// }
+// console.log(counter)
+
+// 20-misol
+// let son1 = +prompt("1-son")
+// let son2 = +prompt("2-son")
+// let max;
+// let min;
+// if (son1>son2){
+//   max =son1
+//   min =son2
+// }else{
+//   max =son2
+//   min=son1
+// }
+// let sum = 0;
+// for (let i = min; i<=max; i++){
+//     sum= sum+i
+// }
+// console.log(sum);
+
+// 21-misol
+// for (let i = 1; i<=50; i++){
+//     if (!(i%3==0)){
+//         console.log(i)
+//     }
+// }
+
+// 22-misol
+// for (let i = 1; i<=20; i++){
+//     if (i%2==0){
+//         console.log("Juft son");
+//     }else{
+//         console.log(i);
+//     }
+// }
+
+// 23-misol
+// let sum= 0;
+// for (let i = 1; i<=100; i++){
+//     if (!(i%10==0))
+//         sum= sum+i
+// }
+// console.log(sum)
+
+// 24-misol
+// let k=1;
+// for (let i = 1; i<=30; i++){
+//     k=k*i
+// }
+// console.log(k);
+
+// 25-misol
+// let son1 = +prompt("1-son")
+// let son2 = +prompt("2-son")
+// for (let i = son1; i<=son2; i++){
+//     console.log(i)
+// }
+
+// 26-misol
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 5 == 0 && i % 3 == 0) {
+//     console.log(i);
+//   }
+// }
+
+// 27-misol
+// let son = +prompt("son")
+// let sum = 1;
+// for ( let i = 1; i<=son; i++){
+//   sum=sum*i
+// }
+// console.log(sum)
+
+// 28-misol
+// for (let i = 1; i<=50; i++){
+//   if (i%2==0){
+//     console.log(i)
+//   }
+// }
+
+// 29-misol
+// for (let i = 1; i<=10; i++){
+//   console.log(i**3)
+// }
+
+// 30-misol
+// let kvadrat =1;
+// for (let i = 1; i<=20; i++){
+//   console.log(i)
+//   kvadrat=i**2
+//   console.log(`Bu sonning kvadrati: ${kvadrat}`);
+// }
+
+// 31-misol
+// let sum= 1;
+// for (let i = 1; i <= 100; i++){
+//   if (i%7==0)
+//     sum=sum*i
+// }
+// console.log(sum);
+
+// 32-misol
+// for (let i = 1; i <= 30; i++){
+//   console.log(i*10);
+  
+// }
+
+// 33-misol
+// XATO MISOL
+
+// 34-misol
+// let son = +prompt("son");
+// let sum = 0;
+// for (let i = 2; i <=son; i+= 2){
+//   if (son>0)
+//     sum= sum+i
+// }
+// console.log(sum);
+
+// 35-misol
+// let son1= +prompt("1-son");
+// let son2 = +prompt("2-son");
+// let sum = 1;
+// for (let i = son1; i <=son2; i++){
+//   sum = sum*i
+// }
+
+// 36-misol
+// let son = +prompt("son");
+// for (let i = 1; i <=son; i++){
+//   if (i>0 && i%10==0){
+//     console.log(i);
+//   }
+// }
+
+// 37-misol
+// let sum= 0;
+// let k= 1;
+// for (let i = 1; i <=15; i++){
+//     k=i**2
+//     console.log(k);
+//     sum=sum+k
+    
+// }
+// console.log(sum);
+
+// 38-misol
+// let factr=1;
+// for (let i = 1; i <=10; i++){
+//   factr= factr*i
+//   console.log(factr);
+  
+// }
+
+// 39-misol
+// let son = +prompt("son");
+// let sum = 0;
+// let k=1;
+// for (let i = 1; i <=son; i++){
+//   k=i**2
+//   console.log(k);
+//   sum=sum+k
+// }
+// console.log(sum);
+
+// 40-misol
+//  let son = +prompt("son");
+//  for (let i = son-1; i >=1; i--){
+  
+//     console.log(i);
+//  }
+
+// 41-misol
+// for ( let i = 2; i<=50; i+=2){
+//     console.log(i**3)
+// }
+
+// 42-misol
+//  for ( let i = 1; i<=100;  i++){
+//     if (!(i%5==0)){
+//         console.log(i);
+//     }
+//  }
+
+// 43-misol
+// let num = +prompt("sonni kiriting")
+//  for ( let i = 1; i<=num; i++){
+//     if (i%3==0 ){
+//         console.log(i);
+//     }
+//  }
+
+// 44-misol
+//  for ( let i = 1; i<=20; i++){
+//     if (i%2==1){
+//         console.log("Toq son");
+//     }
+//     if (i%2==0){
+//         console.log("Juft son");
+//     }
+//  }
+
+// 45-misol
+// let sum = 0;
+//  for ( let i = 1; i<=10; i++){
+//     console.log(i**2);
+//       sum +=i
+//  }
+// console.log(sum);
+
+// 47-misol
+// let sum = 0;
+//  for ( let i = 1; i<=30; i++){
+//     if (i%3==0 && i%4==0){
+//         console.log(i);
+//     }
+//     sum += i
+//  }
+//  console.log(sum);
+ 
+// 48-misol
+//  for ( let i = 1; i<=50; i++){
+//     if (i%2==1){
+//         console.log(i);
+//     }
+//  }
+
+// 49-misol
+// let num = + prompt("sonni kiriting")
+// let k = 1;
+//  for ( let i = 1; i<=num; i++){
+//     if (i%7==0){
+//         console.log(i);
+//         k = k * i;
+//     }
+//  }
+//  console.log(k);
+
+// 50-misol
+//  let num = +prompt("sonni kiriting");
+//  let num2 = +prompt("sonni kiriting");
+//  let sum = 0;
+//  for ( let i = num; i<=num2; i++){
+//         console.log(i**2);
+//          sum = sum + i;
+//  }
+// console.log(sum);
+ 
